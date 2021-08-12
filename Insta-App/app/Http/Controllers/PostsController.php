ef0e40ada8c65b5561ec917026ce065438b5cc38
Tobi008 <55408828+Tobi008@users.noreply.github.com>
2021-08-12 14:50:34 -0500

    user can now make posts to their feed

--- Insta-App/app/Http/Controllers/PostsController.php
+++ Insta-App/app/Http/Controllers/PostsController.php
@@ -24,7 +24,13 @@ class PostsController extends Controller
             'image' => 'required|image'
         ]);
 
-        auth()->user()->posts()->create($data);
-        dd(request()->all());
+        $imagePath = request('image')->store('uploads','public');
+
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);
+
+        return redirect('/profile/'. auth()->user()->id);
     }
 }
--- Insta-App/resources/views/profiles/index.blade.php
+++ Insta-App/resources/views/profiles/index.blade.php
@@ -9,7 +9,7 @@
            <div class="col-9 pt-5">
                <div class="d-flex justify-content-between align-items-baseline">
                    <h1>{{$user->username}}</h1>
-                   <a href=""> Add a Post </a>
+                   <a href="/p/create"> Add a Post </a>
 
                 </div>
                <div class="d-flex">
@@ -25,15 +25,11 @@
    </div>
 
    <div class="row pt-5">
-       <div class="col-4">
-        <img src="https://i.pinimg.com/originals/30/2b/f8/302bf8c7124220acc03e17fbaaf3c47b.jpg" class="w-100">
+      @foreach ( $user->posts as $post)
+      <div class="col-4">
+        <img src= "/storage/{{$post->image}}" class="w-100">
        </div>
-       <div class="col-4">
-        <img src="https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" class="w-100">
-       </div>
-       <div class="col-4">
-        <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--__El_3m5--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/64589gqyn00oxrai14gp.jpg" class="w-100">
-       </div>
+      @endforeach
    </div>
 </div>
 @endsection
