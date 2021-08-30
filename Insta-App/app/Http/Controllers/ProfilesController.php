becc31bb719e706c4189787f6952dfa9f1f1ece8
Tobi008 <55408828+Tobi008@users.noreply.github.com>
2021-08-30 14:11:15 -0500

    Default profile created for users after registering on site using policy/ edited the front end layout of viewing posts

--- Insta-App/app/Http/Controllers/ProfilesController.php
+++ Insta-App/app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -17,12 +18,15 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user->profile);
         return view('profiles.edit', [
             'user' => $user,
         ]);
     }
     public function update(User $user)
     {
+        $this->authorize('update', $user->profile);
+
         $data = request()->validate([
             'title' => 'required',
             'bio' => 'required',
@@ -30,7 +34,16 @@ class ProfilesController extends Controller
             'image' => '',
         ]);
 
-        auth()->user()->profile->update($data);
+        if(request('image')){
+            $imagePath = request('image')->store('profile','public');
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
+            $image->save();
+        }
+
+        auth()->user()->profile->update(array_merge(
+            $data, 
+            ['image' => $imagePath]
+        ));
 
         return redirect("/profile/{$user->id}");
     }
